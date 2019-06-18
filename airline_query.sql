ecfdba0951dc2a356df6ba04837c64aa08907ff2
SolidifiedRay <ray2147483647@outlook.com>
2019-06-18 20:14:47 +0800

    add some query example

---
+++ airline_query.sql
@@ -0,0 +1,19 @@
+#1.
+SELECT *
+FROM Flight
+WHERE d_date_time > NOW()
+
+#2.
+SELECT*
+FROM Flight
+WHERE status = "delayed"
+
+#3.
+SELECT Customer.c_name
+FROM Customer, Ticket
+WHERE Customer.c_email = Ticket.c_email
+
+#4.
+SELECT *
+FROM Airplane
+WHERE al_name = "China Eastern"
